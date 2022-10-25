0d46f3d52a5d207e93f6ee0f7e12b49108d40ce3
Subhendra Basu <basu.subhendra@outlook.com>
2022-10-25 14:24:42 +0530

    code

--- _factorize_.cpp
+++ _factorize_.cpp
@@ -14,6 +14,7 @@ using namespace boost;
 #define MAGIC 18
 unsigned long long int offset = 0;
 char* num = 0;
+char* rnum = 0;
 struct arg {
 	char* num;
 	int t;
@@ -85,97 +86,11 @@ char* _int_(std::string x) {
 }
 
 void* factorize(void* _arg_) {
-	const char state_matrix[MAGIC] = { '1', '1', '1', '1', '0', '0', '1', '1', '1', '0', '0', '1', '1', '0', '0', '1', '0', '0' };
-	struct arg* arg1 = (struct arg*) _arg_;
-	int param = arg1->t;
-	unsigned long long int offset = arg1->offset;
-	char* return_string = (char*) calloc(MAGIC + 1, sizeof(char));
-	FILE* f = 0;
-	if (param == 0) {
-		f = fopen64("./pi.txt","r");
-	} else {
-		f = fopen64("./e.txt","r");
-	}
-	fseek(f, offset, SEEK_SET);
-	unsigned long l = strlen(num);
-	for (int i = 0; i < MAGIC; ++i) {
-		char pp = 0;
-		fscanf(f, "%c", &pp);
-		if (pp == num[(i + offset) % l]) {
-			return_string[i] = state_matrix[i];
-		} else {
-			return_string[i] = '*';
-		}
-	}
-	return return_string;
-}
-
-void* characterize(char* num, vector<char*>& triplets) {
-	unsigned long int ctr = 0;
-	unsigned long long int l = strlen(num);
-	if (l <= 3) {
-		triplets.push_back(strdup(num));
-		return 0;
-	}
-	while (1) {
-		char* triplet = (char*) calloc(4, sizeof(char));
-		for (int i = ctr; i < ctr + 3; ++i) {
-			triplet[i-ctr] = num[i];
-		}
-		triplets.push_back(triplet);
-		if ((ctr + 3) == l) {
-			break;
-		}
-		ctr++;
-	}
-	return 0;
 }
 
 int main(int argc, char* argv[]) {
 	num = strdup(argv[1]);
-	vector<char*> triplets;
-	characterize(num, triplets);
-	struct arg* arg1 = (struct arg*) calloc(1, sizeof(struct arg));
-	struct arg* arg2 = (struct arg*) calloc(1, sizeof(struct arg));
-	arg1->t = 0;
-	arg2->t = 1;
-	pthread_t thread_id1, thread_id2;
-	unsigned long long int ctr = 0;
-	unsigned long long int sz = triplets.size();
-	while (ctr < sz) {
-		arg2->num = arg1->num = triplets[ctr];
-		arg1->offset = offset;
-		arg2->offset = offset;
-		char* ret1 = 0, *ret2 = 0;
-                pthread_create(&thread_id1, NULL, factorize, arg1);
-		pthread_create(&thread_id2, NULL, factorize, arg2);
-		pthread_join(thread_id1, (void**) &ret1);
-		pthread_join(thread_id2, (void**) &ret2);
-		cout << "ret1: " ;
-		for (int i = 0; i < MAGIC; ++i ) {
-			if (ret1[i] != '*') {
-				cout << ret1[i]  << " , ";
-			} else {
-				cout  << "  , " ;
-			}
-		}
-		cout << endl;
-		cout << "ret2: " ;
-		for (int i = 0; i < MAGIC; ++i ) {
-			if (ret2[i] != '*') {
-				cout << ret2[i] << " , ";
-			} else {
-				cout << "  , ";
-			}
-		}
-		cout << endl;
-                free(ret1);
-		free(ret2);
-		offset += MAGIC;
-		++ctr;
-	}
-	free(arg1);
-	free(arg2);
+	rnum = strrev(num);
 	return 0;
 }
 
